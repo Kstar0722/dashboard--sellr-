647384f063ca9a382b5da13ea95c82c32313856a
Bruce van Zyl <bruce@trumedia.io>
2016-02-28 21:20:52 -0500

    Fixed width for whole site...  new global variables for easy changes

--- modules/users/client/controllers/manager/dashboard.client.controller.js
+++ modules/users/client/controllers/manager/dashboard.client.controller.js
@@ -13,6 +13,7 @@ angular.module('users.manager').controller('DashboardController', ['$scope', '$s
             [65, 59, 80, 81, 56, 55, 40],
             [28, 48, 40, 19, 86, 27, 90]
         ];
+
         $scope.onClick = function (points, evt) {
             console.log(points, evt);
         };
