9b8ad7c89ce3fb596fa7623cce880a9d8177d111
Ross Palmer <rpalmer@expertoncue.com>
2016-05-06 11:27:59 -0400

    intercom service works on log in and page refresh.

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -1,36 +1,16 @@
 
 'use strict';
 
-angular.module('core').controller('HeaderController', [ '$scope', 'Authentication', 'Menus', '$http', '$window', '$state', 'intercomService',
-    function ($scope, Authentication, Menus, $http, $window, $state, intercomService) {
+angular.module('core').controller('HeaderController', [ '$scope', 'Authentication', 'Menus', '$http', '$window', '$state',
+    function ($scope, Authentication, Menus, $http, $window, $state) {
         $scope.authentication = Authentication;
         $scope.ui = {};
         $scope.$state = $state;
-        var called = false;
+
         var originatorEv;
         $scope.isCollapsed = false;
         $scope.menu = Menus.getMenu('topbar');
         console.log('menus %O', $scope.menu);
-        if($scope.authentication.user)
-            intercomService.intercomActivation();
-
-        //
-        //
-        //var user = {{ user | json | safe }};
-        //
-        //
-        //
-        //window.intercomSettings = {
-        //    app_id: "ugnow3fn",
-        //    name: '{{user.displayName}}', // Full name
-        //    email: '{{user.email}}', // Email address
-        //    created_at:'{{user.created | json | safe}}'// Signup date as a Unix timestamp
-        //};
-        //(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var
-        // d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var
-        // s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/ugnow3fn';var
-        // x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
-        //
 
 
         $scope.toggleCollapsibleMenu = function () {
@@ -45,29 +25,7 @@ angular.module('core').controller('HeaderController', [ '$scope', 'Authenticatio
             localStorage.clear();
             $window.localStorage.clear();
             $window.location.href = '/auth/signout';
-
-            // $http.get('/auth/signout')
-            //     .success(function () {
-            //         window.localStorage.clear();
-            //         localStorage.clear();
-            //         $window.localStorage.clear();
-            //
-            //         $window.location.href = '/';
-            //     })
-            //     .error(function (err) {
-            //         console.log('error', err);
-            //     })
         };
 
-        //$scope.$watch('ui.toolbarOpened', function (opened) {
-        //    if (!opened) {
-        //        $scope.ui.toolbarOpened = true;
-        //    }
-        //});
-
-        // Collapsing the menu after navigation
-        //$scope.$on('$stateChangeSuccess', function () {
-        //    $scope.isCollapsed = false;
-        //});
     }
 ]);
--- public/dist/application.js
+++ public/dist/application.js
@@ -403,36 +403,16 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 
 'use strict';
 
-angular.module('core').controller('HeaderController', [ '$scope', 'Authentication', 'Menus', '$http', '$window', '$state', 'intercomService',
-    function ($scope, Authentication, Menus, $http, $window, $state, intercomService) {
+angular.module('core').controller('HeaderController', [ '$scope', 'Authentication', 'Menus', '$http', '$window', '$state',
+    function ($scope, Authentication, Menus, $http, $window, $state) {
         $scope.authentication = Authentication;
         $scope.ui = {};
         $scope.$state = $state;
-        var called = false;
+
         var originatorEv;
         $scope.isCollapsed = false;
         $scope.menu = Menus.getMenu('topbar');
         console.log('menus %O', $scope.menu);
-        if($scope.authentication.user)
-            intercomService.intercomActivation();
-
-        //
-        //
-        //var user = {{ user | json | safe }};
-        //
-        //
-        //
-        //window.intercomSettings = {
-        //    app_id: "ugnow3fn",
-        //    name: '{{user.displayName}}', // Full name
-        //    email: '{{user.email}}', // Email address
-        //    created_at:'{{user.created | json | safe}}'// Signup date as a Unix timestamp
-        //};
-        //(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var
-        // d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var
-        // s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/ugnow3fn';var
-        // x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
-        //
 
 
         $scope.toggleCollapsibleMenu = function () {
@@ -447,30 +427,8 @@ angular.module('core').controller('HeaderController', [ '$scope', 'Authenticatio
             localStorage.clear();
             $window.localStorage.clear();
             $window.location.href = '/auth/signout';
-
-            // $http.get('/auth/signout')
-            //     .success(function () {
-            //         window.localStorage.clear();
-            //         localStorage.clear();
-            //         $window.localStorage.clear();
-            //
-            //         $window.location.href = '/';
-            //     })
-            //     .error(function (err) {
-            //         console.log('error', err);
-            //     })
         };
 
-        //$scope.$watch('ui.toolbarOpened', function (opened) {
-        //    if (!opened) {
-        //        $scope.ui.toolbarOpened = true;
-        //    }
-        //});
-
-        // Collapsing the menu after navigation
-        //$scope.$on('$stateChangeSuccess', function () {
-        //    $scope.isCollapsed = false;
-        //});
     }
 ]);
 ;
@@ -2092,8 +2050,8 @@ angular.module('users.admin').controller('UserController', ['$scope', '$state', 
 ;
 'use strict';
 
-angular.module('users').controller('AuthenticationController', ['$scope', '$state', '$http', '$location', '$window', 'Authentication', 'PasswordValidator', 'constants', 'toastr', 'authToken','intercomService',
-    function ($scope, $state, $http, $location, $window, Authentication, PasswordValidator, constants, toastr, authToken, intercomService) {
+angular.module('users').controller('AuthenticationController', ['$scope', '$state', '$http', '$location', '$window', 'Authentication', 'PasswordValidator', 'constants', 'toastr', 'authToken',
+    function ($scope, $state, $http, $location, $window, Authentication, PasswordValidator, constants, toastr, authToken) {
         $scope.reset = false;
         $scope.authentication = Authentication;
         $scope.popoverMsg = PasswordValidator.getPopoverMsg();
@@ -2240,16 +2198,13 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
             localStorage.setItem('userObject', JSON.stringify({displayName:response.data.displayName, email: response.data.email, created:response.data.created}));
 
             toastr.success('Welcome to the OnCue Dashboard', 'Success');
-            //intercomService.intercomActivation();
             console.log('Authetication.user %s', Authentication.user.roles.indexOf('admin'), Authentication.user.roles.indexOf('manager'), Authentication.user.roles.indexOf('owner'))
             if (Authentication.user.roles.indexOf('manager') < 0 && Authentication.user.roles.indexOf('owner') < 0 && Authentication.user.roles.indexOf('admin') < 0) {
                 if (Authentication.user.roles.indexOf('editor') >= 0) {
                     $state.go('editor.products', { type: "wine", status: "new" })
-                    //$window.location.reload();
                 }
             } else {
                 $state.go('dashboard', $state.previous.params);
-                //$window.location.reload();
             }
 
         }
@@ -4834,7 +4789,7 @@ angular.module('users').directive('lowercase', function () {
   };
 });
 ;
-angular.module('users').service('accountsService', function ($http, constants, toastr) {
+angular.module('users').service('accountsService', function ($http, constants, toastr, intercomService) {
     var me = this;
 
 
@@ -4861,6 +4816,7 @@ angular.module('users').service('accountsService', function ($http, constants, t
                 }
                 if (account.accountId == me.selectAccountId) {
                     me.currentAccount = account;
+                    intercomService.intercomActivation();
                     console.log('setting current account %O', me.currentAccount)
                 }
             });
