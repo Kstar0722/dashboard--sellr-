93745d3eff6733aac62140a388ba1fdcd8db1fa0
Ross van Zyl <rossvz@getsellr.com>
2016-07-08 17:43:38 -0400

    updated domains to sllr.io

--- modules/core/client/app/init.js
+++ modules/core/client/app/init.js
@@ -32,9 +32,9 @@ angular.module(ApplicationConfiguration.applicationModuleName).config([ '$locati
         envServiceProvider.config({
             domains: {
                 local: [ 'localhost' ],
-                development: [ 'dashdev.expertoncue.com', 'dashdev.sllr.io', 'sellrdashboard-dev.herokuapp.com' ],
-                staging: [ 'dashqa.expertoncue.com', 'dashqa.sllr.io', 'dashboard.sllr.io', 'sellrdashboard-staging.herokuapp.com' ],
-                production: [ 'dashboard.expertoncue.com', 'www.sellrdashboard.com', 'sellrdashboard.com', 'dashboard.sellr.io', 'sellrdashboard.herokuapp.com'],
+                development: [ 'dashdev.sllr.io' ],
+                staging: [ 'dashqa.sllr.io', 'dashboard.sllr.io' ],
+                production: [ 'www.sellrdashboard.com', 'sellrdashboard.com', 'dashboard.sellr.io' ],
             },
             vars: {
                 local: {
@@ -45,17 +45,17 @@ angular.module(ApplicationConfiguration.applicationModuleName).config([ '$locati
 
                 development: {
                     API_URL: 'https://apidev.sllr.io',
-                    BWS_API: 'https://sellr-bws-dev.herokuapp.com',
+                    BWS_API: 'https://bwsdev.sllr.io',
                     env:'dev'
                 },
                 staging: {
                     API_URL: 'https://apiqa.sllr.io',
-                    BWS_API: 'https://sellr-bws-staging.herokuapp.com',
+                    BWS_API: 'https://dashqa.sllr.io',
                     env:'staging'
                 },
                 production: {
-                    API_URL: 'https://api.expertoncue.com',
-                    BWS_API: 'https://tv.sllr.io',
+                    API_URL: 'https://api.sllr.io',
+                    BWS_API: 'https://bws.sllr.io',
                     env:'production'
                 }
             }
