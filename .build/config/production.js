62cc8c36d21ad6b2bac8e9ae6a7b36927b4b254a
Ross van Zyl <rossvz@getsellr.com>
2016-07-29 09:47:14 -0400

    fixed menu service for non-admins

--- .build/config/production.js
+++ .build/config/production.js
@@ -8,7 +8,7 @@ module.exports = {
         },
         css: 'public/dist/application.min.8f66ec.css',
         js: [
-            'public/dist/application.min.dd450e.js',
+            'public/dist/application.min.e60c81.js',
             'public/dist/templates.min.17ad0d.js'
         ]
     }
