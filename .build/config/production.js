995632ece8b6df94a864dc032ebb5b2b4723b576
Ross van Zyl <rossvz@getsellr.com>
2016-08-02 16:38:49 -0400

    more merging

---
+++ .build/config/production.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    client: {
+        lib: {
+            css: 'public/dist/lib.min.58dfca.css',
+            js: 'public/dist/lib.aa735e.js'
+        },
+        css: 'public/dist/application.min.975d96.css',
+        js: [
+            'public/dist/application.min.d2b2d7.js',
+            'public/dist/templates.min.1d0c2d.js'
+        ]
+    }
+};
