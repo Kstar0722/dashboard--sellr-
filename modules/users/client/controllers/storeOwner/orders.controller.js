c2a09e73a46712368d954a01460f85bc96a8f551
Diego Fortes <daigof@gmail.com>
2016-08-09 13:52:25 -0300

    initla logic

--- modules/users/client/controllers/storeOwner/orders.controller.js
+++ modules/users/client/controllers/storeOwner/orders.controller.js
@@ -1,12 +1,24 @@
 'use strict'
-/* global angular */
+/* global angular, moment, _ */
 angular.module('users.admin').controller('StoreOwnerOrdersController', [ '$scope', 'Authentication', '$filter', 'Admin', '$http', '$state', 'CurrentUserService', 'constants', 'accountsService', 'toastr',
   function ($scope, Authentication, $filter, Admin, $http, $state, CurrentUserService, constants, accountsService, toastr) {
     var API_URL = constants.API_URL
+    $scope.todayOrders = []
+    $scope.pastOrders = []
+
     function getOrders () {
       var ordersUrl = API_URL + '/mobile/reservations/store/' + 1269
       $http.get(ordersUrl).then(function (response) {
-        console.log('response ORDERSSSSS', response)
+        console.log('Response Orders From Store', response.data)
+        var allOrders = response.data
+        $scope.todayOrders = _.filter(allOrders, function (order) { return moment().isSame(order.pickupTime, 'day') })
+        $scope.pastOrders = _.filter(allOrders, function (order) { return moment().isAfter(order.pickupTime, 'day') })
+        _.each($scope.todayOrders, function (order) {
+          order.uiHour = 2
+          order.uiTime = 'PM'
+          order.uiCreated = 'July 29, 2016 8:00PM'
+        })
+        console.log('today orders', $scope.todayOrders)
       })
     }
     getOrders()
