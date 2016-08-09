9b5898aa75f9583455149c1651b0351441d0c30f
Ross van Zyl <rossvz@getsellr.com>
2016-08-09 17:10:52 -0400

    added checkbox for Shoppr app on account manager page - need to still enforce on app side

--- modules/users/client/controllers/admin/accountManager.client.controller.js
+++ modules/users/client/controllers/admin/accountManager.client.controller.js
@@ -1,41 +1,42 @@
+/* globals angular*/
 angular.module('users.admin').controller('AccountManagerController', function ($scope, locationsService, $state, accountsService, CurrentUserService, Authentication, $http, constants, uploadService, toastr) {
-    accountsService.init();
-    $scope.accountsService = accountsService;
-    $scope.determinateValue = 0;
-    $scope.accountLogo = '';
-    $scope.account = {
-        createdBy: ''
-    };
-    if (Authentication.user) {
-        $scope.account.createdBy = Authentication.user.username
+  accountsService.init()
+  $scope.accountsService = accountsService
+  $scope.determinateValue = 0
+  $scope.accountLogo = ''
+  $scope.account = {
+    createdBy: ''
+  }
+  if (Authentication.user) {
+    $scope.account.createdBy = Authentication.user.username
+  }
+  console.log($scope.account)
+
+  // changes the view, and sets current edit account
+  $scope.editAccount = function (account) {
+    console.log('editing account %O', account)
+    $scope.currentAccountLogo = ''
+    accountsService.editAccount = account
+    accountsService.editAccount.shoppr = Boolean(JSON.parse(account.preferences).shoppr)
+    console.log('editAccount is now %O', accountsService.editAccount)
+    $state.go('admin.accounts.edit', { id: account.accountId })
+    window.scrollTo(0, 0)
+  }
+
+  $scope.upload = function (files, accountId) {
+    var mediaConfig = {
+      mediaRoute: 'media',
+      folder: 'logo',
+      type: 'LOGO',
+      accountId: accountId
     }
-    console.log($scope.account);
+    uploadService.upload(files[ 0 ], mediaConfig).then(function (response, err) {
+      if (response) {
+        accountsService.editAccount.logo = constants.ADS_URL + 'logo/' + response[ 0 ].mediaAssetId + '-' + response[ 0 ].fileName
+        $scope.currentAccountLogo = accountsService.editAccount.logo
+        toastr.success('Logo Updated', 'Success!')
+      }
+    })
+  }
 
-    //changes the view, and sets current edit account
-    $scope.editAccount = function (account) {
-        console.log('editing account %O', account)
-        $scope.currentAccountLogo = '';
-        accountsService.editAccount = account;
-        //accountsService.editAccount.style = JSON.parse(account.preferences).style
-        console.log('editAccount is now %O', accountsService.editAccount)
-        $state.go('admin.accounts.edit', {id: account.accountId})
-    }
-
-
-    $scope.upload = function (files, accountId) {
-        var mediaConfig = {
-            mediaRoute: 'media',
-            folder:'logo',
-            type:'LOGO',
-            accountId: accountId
-        }
-        uploadService.upload(files[0], mediaConfig).then(function(response, err ){
-            if(response) {
-                accountsService.editAccount.logo = constants.ADS_URL + 'logo/'+response[0].mediaAssetId + '-' + response[0].fileName;
-                $scope.currentAccountLogo = accountsService.editAccount.logo;
-                toastr.success('Logo Updated', 'Success!');
-            }
-        })
-    };
-
-});
+})
