80640469d67c574ea35c02a62bf8bfbafd226ac3
Diego Fortes <daigof@gmail.com>
2016-08-18 15:21:42 -0300

    redesign orders Status selector

---
+++ modules/users/client/filters/stringToClassFilter.js
@@ -0,0 +1,10 @@
+/*globals angular */
+angular.module('users')
+  .filter('stringToClass', function () {
+    return function (input) {
+      if (typeof input !== 'string') {
+        return ''
+      }
+      return input.trim().toLowerCase().replace(/\s/g, '-')
+    }
+  })
