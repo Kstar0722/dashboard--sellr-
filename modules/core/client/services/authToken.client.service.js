da4d0d07c5841599c6872ed778d60123f3a10f91
Diego Fortes <daigof@gmail.com>
2016-08-19 12:37:03 -0300

    user authentication routing Fix redirect to LOGIN

--- modules/core/client/services/authToken.client.service.js
+++ modules/core/client/services/authToken.client.service.js
@@ -1,39 +1,45 @@
+/* global angular */
 angular.module('core').factory('authToken', function ($window) {
+  var me = this
+  var storage = $window.localStorage
+  var cachedToken
+  var userToken = 'token'
 
-  var me = this;
-  var storage = $window.localStorage;
-  var cachedToken;
-  var userToken = 'token';
-
-// Save Token to Storage as 'token'
-  function setToken(token) {
-    cachedToken = token;
-    storage.setItem(userToken, token);
-
+  // Save Token to Storage as 'token'
+  function setToken (token) {
+    cachedToken = token
+    storage.setItem(userToken, token)
   }
 
-// Get token 'token' from storage
-  function getToken() {
-    if (!cachedToken)
-      cachedToken = storage.getItem(userToken);
+  // Get token 'token' from storage
+  function getToken () {
+    if (!cachedToken) {
+      cachedToken = storage.getItem(userToken)
+    }
     return cachedToken
   }
 
-// Returns true or false based on whether or not token exists in storage
-  function isAuthenticated() {
-    return !!getToken();
+  // Returns true or false based on whether or not token exists in storage
+  function isAuthenticated () {
+    return !!getToken()
   }
 
-//Removes token
-  function removeToken() {
-    cachedToken = null;
+  // Returns true or false based on whether or not token exists in storage
+  function hasTokenInStorage () {
+    return storage.getItem(userToken) !== null
+  }
+
+  // Removes token
+  function removeToken () {
+    cachedToken = null
     storage.removeItem(userToken)
   }
 
-  me.setToken = setToken;
-  me.getToken = getToken;
-  me.isAuthenticated = isAuthenticated;
-  me.removeToken = removeToken;
+  me.setToken = setToken
+  me.getToken = getToken
+  me.isAuthenticated = isAuthenticated
+  me.removeToken = removeToken
+  me.hasTokenInStorage = hasTokenInStorage
 
-  return me;
-});
+  return me
+})
