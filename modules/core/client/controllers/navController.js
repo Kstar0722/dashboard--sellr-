f05c1f564aa62c9a1cd91a621a3fcd74ab997c9b
Diego Fortes <daigof@gmail.com>
2017-03-08 19:39:14 -0300

    more design - logic and custom Account menu

--- modules/core/client/controllers/navController.js
+++ modules/core/client/controllers/navController.js
@@ -1,6 +1,8 @@
 angular.module('core')
 .controller('NavController', [ '$scope', 'Authentication', 'Menus', '$http', '$window', '$state', '$stateParams', 'accountsService', 'constants', 'authenticationService', function ($scope, Authentication, Menus, $http, $window, $state, $stateParams, accountsService, constants, authenticationService) {
   $scope.ui = {}
+  $scope.ui.accountOptionsLabel = Authentication.user.displayName
+
   $scope.accountsService = accountsService
 
   $scope.signOut = function () {
@@ -8,26 +10,33 @@ angular.module('core')
   }
 
   $scope.accountChangeHandler = function (account) {
-    console.log(account)
-    // $scope.$root.selectAccountId = account.accountId
-    // $scope.mobileMenuActive.open = false
-    // localStorage.setItem('accountId', account.accountId)
+    $scope.$root.selectAccountId = account.accountId
+    localStorage.setItem('accountId', account.accountId)
   }
 
   $scope.shouldRenderPrimaryItem = function (item) {
     switch (item) {
-      case 'dash':
+      case 'admin':
         return Authentication.userInRole('admin')
       default:
         return true
     }
   }
-  $scope.shouldRenderSecondaryItem = function (item) {
-    switch (item) {
-      case 'dash':
-        return Authentication.userInRole('admin')
-      default:
-        return true
+
+  $scope.$root.$on('$stateChangeSuccess', function (e, toState, toParams) {
+    if (!toState.name.match(/^(dashboard|storeOwner.orders|manager.ads|settings|editProfile|productsUploader|websiteBuilder)/i)) {
+      $scope.$root.selectAccountId = null
+    } else if (toState) {
+      toParams.accountId = $scope.$root.selectAccountId
+      $state.go(toState.name, toParams, {notify: false})
     }
+    setActiveRoute(toState.name)
+  })
+
+  function setActiveRoute (state) {
+    if (state.indexOf('supplier') > -1) { $scope.ui.primaryRoute = 'supplier' }
+    if (state.indexOf('admin') > -1) { $scope.ui.primaryRoute = 'admin' }
+    if (state.indexOf('editor') > -1 || _.contains(['productHistory', 'curator.store'], state)) { $scope.ui.primaryRoute = 'editor' }
+    if (_.contains(['dashboard', 'productsUploader', 'websiteBuilder', 'manager.ads', 'storeOwner.orders'], state)) { $scope.ui.primaryRoute = 'store' }
   }
 }])
