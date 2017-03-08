c204bcf2808986c37979e529ce63910d54c0dfa6
Diego Fortes <daigof@gmail.com>
2017-03-08 13:10:26 -0300

    1503: initial logic

--- modules/core/client/controllers/navController.js
+++ modules/core/client/controllers/navController.js
@@ -1,4 +1,33 @@
 angular.module('core')
 .controller('NavController', [ '$scope', 'Authentication', 'Menus', '$http', '$window', '$state', '$stateParams', 'accountsService', 'constants', 'authenticationService', function ($scope, Authentication, Menus, $http, $window, $state, $stateParams, accountsService, constants, authenticationService) {
-  console.log('DIEGOO')
+  $scope.ui = {}
+  $scope.accountsService = accountsService
+
+  $scope.signOut = function () {
+    authenticationService.signout()
+  }
+
+  $scope.accountChangeHandler = function (account) {
+    console.log(account)
+    // $scope.$root.selectAccountId = account.accountId
+    // $scope.mobileMenuActive.open = false
+    // localStorage.setItem('accountId', account.accountId)
+  }
+
+  $scope.shouldRenderPrimaryItem = function (item) {
+    switch (item) {
+      case 'dash':
+        return Authentication.userInRole('admin')
+      default:
+        return true
+    }
+  }
+  $scope.shouldRenderSecondaryItem = function (item) {
+    switch (item) {
+      case 'dash':
+        return Authentication.userInRole('admin')
+      default:
+        return true
+    }
+  }
 }])
