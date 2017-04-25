65d9cbd05a79e40262a8f06d9fa939ce7e13e1d5
Diego Fortes <daigof@gmail.com>
2017-04-25 12:43:09 -0300

    1649: marketing screens, global checbox fix, massive less cleanup

--- modules/users/client/controllers/storeOwner/StoreOwnerListedProductsController.js
+++ modules/users/client/controllers/storeOwner/StoreOwnerListedProductsController.js
@@ -1,3 +1,5 @@
 angular.module('core').controller('StoreOwnerListedProductsController', function ($scope, $stateParams, $state) {
   console.log('LISTED PRODUCTS CTRL')
+  $scope.ui = {}
+  $scope.ui.checkmarkplaceholder = false
 })
