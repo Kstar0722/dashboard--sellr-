ff969db50d4642de689e100959e8f3da4f32d36b
Diego Fortes <daigof@gmail.com>
2017-04-27 16:34:02 -0300

    1691 create account

--- modules/core/client/controllers/admin/AccountsManagerController.js
+++ modules/core/client/controllers/admin/AccountsManagerController.js
@@ -1,14 +1,27 @@
 angular.module('core').controller('AccountsManagerController', function ($scope, $state, accountsService, CurrentUserService, Authentication, $http, constants, uploadService, toastr, UsStates, $mdDialog, $timeout, $httpParamSerializer, $rootScope, globalClickEventName) {
   //
-  // DEFINITIONS - INITIALIZATION
+  // DEFINITIONS
   //
   $scope.ui = {}
   $scope.ui.display = 'fulltable'
   $scope.ui.sortExpression = '+name'
   $scope.accountsService = accountsService
   $scope.ui.actionsOptionsSelect = []
-  $scope.UsStates = UsStates
   $scope.ui.currentAccount = {}
+  $scope.stateSelectConfig = {
+    create: false,
+    maxItems: 1,
+    allowEmptyOption: false,
+    valueField: 'abbreviation',
+    labelField: 'name',
+    sortField: 'name',
+    searchField: [ 'name' ]
+  }
+  $scope.stateSelectOptions = UsStates
+
+  //
+  // INITIALIZATION
+  //
   accountsService.init({ expand: 'stores,stats' })
 
   //
@@ -36,46 +49,30 @@ angular.module('core').controller('AccountsManagerController', function ($scope,
     $scope.ui.display = 'createAccount'
   }
 
+  $scope.submitAccount = function (formValid) {
+    $scope.ui.stateRequired = false
+    var flag = formValid
+    if (!$scope.ui.currentAccount.state) {
+      $scope.ui.stateRequired = true
+      flag = false
+    }
+    if (!flag) {
+      setFieldsTouched($scope.accountForm.$error)
+      return false
+    }
+
+    if ($scope.ui.display === 'createAccount') {
+      accountsService.createAccount($scope.ui.currentAccount).then(function (newAccount) {
+        $scope.ui.display = 'fulltable'
+      })
+    }
+    if ($scope.ui.display === 'editAccount') {
+    }
+  }
+
   $scope.openEditAccountSidebar = function (account) {
     $scope.ui.currentAccount = account
     $scope.ui.display = 'editAccount'
-  }
-
-  $scope.setUsState = function (state) {
-    $scope.ui.currentAccount.state = state.abbreviation
-    $scope.ui.currentAccount.stateName = state.name
-    $scope.ui.stateError = false
-  }
-
-  $scope.saveAccount = function (view) {
-    // Unfortunately we need to treat custom select boxes errors separately
-    if (!$scope.ui.currentAccount.state) {
-      $scope.ui.stateError = true
-    } else {
-      $scope.ui.stateError = false
-    }
-    // If form invalid set all controls to touched to show input border
-    if ($scope.accountForm.$invalid) {
-      angular.forEach($scope.accountForm.$error, function (field) {
-        angular.forEach(field, function (errorField) {
-          errorField.$setTouched()
-        })
-      })
-    }
-    if ($scope.ui.currentAccount.state && $scope.accountForm.$valid) {
-      var accountPayload = angular.copy($scope.currentAccount)
-      delete accountPayload.stateName
-      if (view === 'createAccount') {
-        console.log(accountPayload)
-        //
-        // TO BE REVIEWED THIS OLD LOGIC MAY CHANGE
-        //
-        // accountsService.createAccount(accountPayload).then(function (newAccount) {
-        //   var newAccountToEdit = _.findWhere(accountsService.accounts, {accountId: newAccount.accountId})
-        //   if (newAccountToEdit) $scope.editAccount(newAccountToEdit)
-        // })
-      }
-    }
   }
 
   $scope.showManageStoresDialog = function (ev) {
@@ -100,6 +97,14 @@ angular.module('core').controller('AccountsManagerController', function ($scope,
     $scope.ui.stateOptionsSelect = false
     $scope.ui.websiteThemesOptionsSelect = false
     $scope.ui.actionsOptionsSelect = []
+  }
+
+  function setFieldsTouched (formErrors) {
+    angular.forEach(formErrors, function (field) {
+      angular.forEach(field, function (errorField) {
+        errorField.$setTouched()
+      })
+    })
   }
 
   //
