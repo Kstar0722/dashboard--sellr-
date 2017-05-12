d0ded52cea5e6bd9a0810811985a4263c2e9e1ff
Diego Fortes <daigof@gmail.com>
2017-05-12 14:35:50 -0300

    BH51: change box effect when clicked accomodate elements inside

--- modules/core/client/controllers/storeOwner/StoreOwnerPromotedProductsController.js
+++ modules/core/client/controllers/storeOwner/StoreOwnerPromotedProductsController.js
@@ -1,5 +1,4 @@
 angular.module('core').controller('StoreOwnerPromotedProductsController', function ($scope, $stateParams, $state) {
-  console.log('PROMOTED PRODUCT CTRL')
   $scope.ui = {}
   $scope.ui.icon = false
   $scope.ui.promotedProducts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
