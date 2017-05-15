7e639a0b418b03d90528746efd66d1c3d27389a3
Diego Fortes <daigof@gmail.com>
2017-05-15 13:30:38 -0300

    BH70: promote product dialog confirmation

--- modules/core/client/controllers/storeOwner/StoreOwnerListedProductsController.js
+++ modules/core/client/controllers/storeOwner/StoreOwnerListedProductsController.js
@@ -2,7 +2,21 @@ angular.module('core').controller('StoreOwnerListedProductsController', function
   $scope.ui = {}
   $scope.ui.display = 'fulltable'
   $scope.ui.checkmarkplaceholder = false
+  var confirmationDialogOptions = {
+    templateUrl: '/modules/core/client/views/popupDialogs/confirmationDialog.html',
+    autoWrap: true,
+    parent: angular.element(document.body),
+    scope: $scope,
+    preserveScope: true,
+    hasBackdrop: true,
+    clickOutsideToClose: true,
+    escapeToClose: true,
+    fullscreen: true
+  }
 
+  //
+  // SCOPE FUNCTIONS
+  //
   $scope.showUploadCsvDialog = function (ev) {
     $mdDialog.show({
       templateUrl: '/modules/core/client/views/popupDialogs/uploadCsvDialog.html',
@@ -15,5 +29,22 @@ angular.module('core').controller('StoreOwnerListedProductsController', function
       fullscreen: true,
       controller: 'CsvImporterController'
     })
+  }
+
+  $scope.showPromoteProductDialog = function (ev) {
+    $scope.genericDialog = {}
+    $scope.genericDialog.title = 'Promote Product'
+    $scope.genericDialog.body = 'The Product will be promoted'
+    $scope.genericDialog.actionText = 'Confirm'
+    $scope.genericDialog.actionClass = 'common-btn-positive'
+    $scope.genericDialog.action = function () {
+      $scope.ui.promotedProduct = true
+      $scope.closeDialog()
+    }
+    $mdDialog.show(confirmationDialogOptions)
+  }
+
+  $scope.closeDialog = function () {
+    $mdDialog.hide()
   }
 })
