3c4cc44bbb6490c19a6bd1ec5bde12a49b65e99a
Diego Fortes <daigof@gmail.com>
2017-05-18 18:06:13 -0300

    TP1802: edit products initial

--- modules/core/client/services/ProductTypes.js
+++ modules/core/client/services/ProductTypes.js
@@ -1,11 +1,11 @@
 angular.module('core').service('Types', function () {
   return {
-    1: {productTypeId: 1, name: 'Wine', similarNames: 'w,wines'},
-    2: {productTypeId: 2, name: 'Beer', similarNames: 'b,beers,ale,lager'},
-    3: {productTypeId: 3, name: 'Spirits', similarNames: 's,spirit,liqueur'},
-    12: {productTypeId: 12, name: 'Generic', similarNames: 'generic,undefined,unknown'},
-    15: {productTypeId: 15, name: 'Cocktails', similarNames: 'cocktails,cocktail'},
-    16: {productTypeId: 16, name: 'Art', similarNames: 'art'},
-    17: {productTypeId: 17, name: 'Health', similarNames: 'health,vitamins,supplements'}
+    1: {productTypeId: 1, name: 'Wine', similarNames: 'w,wines', sizes: ['750 ml', '1.5 L']},
+    2: {productTypeId: 2, name: 'Beer', similarNames: 'b,beers,ale,lager', sizes: ['12 oz bottle', '12 oz can', '4-pack bottles', '4-pack cans', '6-pack bottles', '6-pack cans', '12-pack bottles', '12-pack cans', '24-pack bottles', '24-pack cans', '750 ml']},
+    3: {productTypeId: 3, name: 'Spirits', similarNames: 's,spirit,liqueur', sizes: ['375 ml', '700 ml', '750 ml', '1.75 L']},
+    12: {productTypeId: 12, name: 'Generic', similarNames: 'generic,undefined,unknown', sizes: []},
+    15: {productTypeId: 15, name: 'Cocktails', similarNames: 'cocktails,cocktail', sizes: []},
+    16: {productTypeId: 16, name: 'Art', similarNames: 'art', sizes: []},
+    17: {productTypeId: 17, name: 'Health', similarNames: 'health,vitamins,supplements', sizes: []}
   }
 })
--- modules/core/client/services/productsService.js
+++ modules/core/client/services/productsService.js
@@ -1,5 +1,5 @@
 /* globals angular, localStorage */
-angular.module('core').service('productsService', function ($http, constants, $q) {
+angular.module('core').service('productsService', function ($http, constants, $q, $httpParamSerializer) {
   var me = this
 
   me.createCategory = function (newCategory) {
@@ -17,8 +17,6 @@ angular.module('core').service('productsService', function ($http, constants, $q
 
   me.getTemplates = function (tag, options) {
     var defer = $q.defer()
-
-    console.log('getting templates tagged with ' + tag)
     $http.get(constants.BWS_API + '/choose/templates?tag=' + tag, options).then(function (res) {
       defer.resolve(res.data)
     })
@@ -26,9 +24,72 @@ angular.module('core').service('productsService', function ($http, constants, $q
     return defer.promise
   }
 
+  me.updatePlanProduct = function (product) {
+    if (!product.planId || !product.productId) { return false }
+    var params = {
+      planId: product.planId,
+      product: product.productId
+    }
+    var payload = {
+      payload: product
+    }
+    var url = constants.BWS_API + '/choose/products?' + $httpParamSerializer(params)
+    return $http.put(url, payload)
+  }
+
   me.getPlanProducts = function (accountId) {
+    var defer = $q.defer()
     var url = constants.BWS_API + '/choose/plans?account=' + accountId
-    return $http.get(url)
+    $http.get(url).then(function (response) {
+      var plans = _.map(response.data, function (plan) { return initPlan(plan) })
+      defer.resolve(plans)
+    })
+    return defer.promise
+  }
+
+  me.buildSlots = function () {
+    var slots = []
+    var letters = [ 'A', 'B', 'C', 'D', 'E', 'F' ]
+    for (var l in letters) {
+      for (var i = 1; i <= 6; i++) {
+        slots.push({slot: letters[ l ] + i})
+      }
+    }
+    return slots
+  }
+
+  function initPlan (plan) {
+    if (!plan) return plan
+    plan.products = plan.products || []
+    _.each(plan.products, function (product) {
+      initProduct(product, plan.planId)
+    })
+    return plan
+  }
+
+  function initProduct (product, planId) {
+    if (!product) return product
+    product.planId = planId
+    product.options = product.options || {}
+    product.price = parseFloat(product.price) || product.price
+    product.oprice = parseFloat(product.oprice) || product.oprice
+    product.researchImage = product.options.researchImage
+
+    if (!product.publicUrl) {
+      var media = _.find(product.mediaAssets, { type: 'IMAGE' })
+      product.publicUrl = media && media.publicUrl
+    }
+
+    if (!_.isEmpty(product.prices)) {
+      var defaultIndex = _.findIndex(product.prices, function (p) {
+        return p.size === product.defaultPriceLabel
+      })
+      if (defaultIndex > -1) {
+        product.prices.unshift(product.prices.splice(defaultIndex, 1)[0])
+      }
+    }
+
+    return product
   }
 
   return me
